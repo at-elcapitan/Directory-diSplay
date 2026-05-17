451d33305d47fa59f1db60021ce98da85425e82c
Vladislav "ElCapitan" Nazaorv <elcapitan@atproject.com.ua>
2026-05-17 14:18:00 +0300

    add: help, version message, ported argument parser

--- directory-display/utils.swift
+++ directory-display/utils.swift
@@ -27,3 +27,65 @@ public func fmt_size(size: Double) -> Format {
         return Format(size: size / GB, shortName: "GB")
     }
 }
+
+public func displayHelp() {
+    print("Usage: ds [OPTION] [DIRECTORY]");
+    print();
+    print("Display directory contents");
+    print();
+    print("Options:");
+    print("  -a, --all             display all directory content");
+    print("  -s, --size            display files size");
+    print("  -f, --full-name       disable file name shortening");
+    print("  -A, --access          display access options");
+    print("      --version         display current program version");
+    print("      --help            display this message)");
+    print();
+    print("Directory diSplay home page: <https://github.com/at-elcapitan/Directory-diSplay>");
+}
+
+public func displayVersion() {
+    print("ds (Directory diSplay) 26.0 for macOS");
+    print();
+    print(
+        "This is free software; see the source for copying conditions. There is NO"
+    );
+    print(
+        "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."
+    );
+    print();
+    print("Written by Vladislav 'ElCapitan' Nazarov");
+}
+
+public func displayFirstLine(
+    maxLength: Int,
+    currDirName: String,
+    currDirNameLen: Int,
+    flagAccess: Bool,
+    flagSize: Bool
+) {
+    var additional: String = ""
+    
+    // TODO: at v26.1
+    // if flagSize {
+    //     additional.append("   SIZE\t\t")
+    // }
+    
+    // if flagAccess {
+    //     additional.append("PUSER\tGROUP\tOTHER")
+    // }
+    
+    let spacing = String(
+        repeating: " ",
+        count: maxLength + 7 - currDirNameLen
+    )
+    
+    print("\(currDirName)/\(spacing)\tTYPE\t\t\(additional)")
+}
+
+
+// public func displayObjects(
+//    objects:
+// ) {
+//
+// }
